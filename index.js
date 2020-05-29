87aeeef138287d2c7fe012a68ae20655a2e2e632
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-28 23:37:30 -0500

    commands created

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+#!/usr/bin/env node
+
 "use strict";
 
 const minimist = require("minimist");
-
-console.log(process.argv);
-
 const argv = minimist(process.argv.slice(2));
 
-console.log(argv);
+async function main() {
+  const command = argv._.shift();
+
+  switch (command) {
+    case "users:create":
+      break;
+    case "users:list":
+      break;
+    case "users:create":
+      break;
+    case "users:list":
+      break;
+    default:
+      console.error(`command not found: ${command}`);
+  }
+}
+
+main().catch((err) => console.log(err));
